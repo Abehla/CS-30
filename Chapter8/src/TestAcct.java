7d67663c5ab7cad809db383a1fe831c75b341f3a
1100015357 <1100015357@L815WD220017.edu.cbe.ab.ca>
2022-03-31 10:03:40 -0600

    ch8

--- Chapter8/src/TestAcct.java
+++ Chapter8/src/TestAcct.java
@@ -5,7 +5,7 @@ public class TestAcct {
 		
 		
 		Personal bank = new Personal(200,"John", "Jones", "Alberta","Calgary", "Seaton", "T1X 02A");
-		business bank =  new business(100, "Bill","Gates", "Alberta", "Edmonton","Westmere", "T1X 07Q");
+		business b =  new business(100, "Bill","Gates", "Alberta", "Edmonton","Westmere", "T1X 07Q");
 		
 		System.out.println(bank.toString() + "\n");
 		
